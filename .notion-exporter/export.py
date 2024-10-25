a5a411bc607db030376d3d55781c86e80e92688f
cploujoux <ch.ploujoux@gmail.com>
2024-10-24 20:34:24 -0700

    Add pr creation

--- .notion-exporter/export.py
+++ .notion-exporter/export.py
@@ -1,7 +1,6 @@
 from python_notion_exporter import NotionExporter, ExportType, ViewExportType
 import os
 
-github_token = os.getenv("GITHUB_TOKEN")
 notion_token = os.getenv("NOTION_TOKEN")
 notion_file_token = os.getenv("NOTION_FILE_TOKEN")
 page = os.getenv("NOTION_PAGE", "111a77bf59b480d9b0ebfe5b3743e768")
@@ -13,7 +12,7 @@ if __name__ == "__main__":
         pages={"Documentation": page},
         export_directory=".notion-exporter",
         export_name="export",
-        flatten_export_file_tree=True,
+        flatten_export_file_tree=False,
         export_type=ExportType.MARKDOWN,
         current_view_export_type=ViewExportType.CURRENT_VIEW,
         include_files=True,
