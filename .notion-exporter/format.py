eb1503ac01a050e9c00843515dfb2f15c0dd7610
cploujoux <ch.ploujoux@gmail.com>
2024-10-27 14:17:29 -0700

    Change replace

--- .notion-exporter/format.py
+++ .notion-exporter/format.py
@@ -79,8 +79,11 @@ def rewrite_title(file_path):
     # Remove the title line from content
     new_content = re.sub(pattern, '', content, 1)
 
-    new_content = new_content.replace("“", '"')
-    new_content = new_content.replace("”", '"')
+    new_content = new_content.replace("“", '\'')
+    new_content = new_content.replace("”", '\'')
+    # Remove empty lines at the start until we hit ---
+    while new_content.startswith('\n'):
+        new_content = new_content[1:]
     # Write the modified content back to the file
     if content != new_content:
       with open(file_path, 'w', encoding='utf-8') as f:
